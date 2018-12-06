4292c2c685e5b32a650d2fea4e04005b0f24fd62
Daniel Kirillov <tldrlol@protonmail.com>
2018-12-06 16:18:58 +0200

    Add controls to fine tune search

--- DK.WallHaven.App/MainViewModelBase.cs
+++ DK.WallHaven.App/MainViewModelBase.cs
@@ -11,16 +11,16 @@
       set => this.Set(ref this.searchQuery, value);
     }
 
-    ObservableCollection<SearchResult> searchResults = new ObservableCollection<SearchResult>();
-    public ObservableCollection<SearchResult> SearchResults {
-      get => this.searchResults;
-      set => this.Set(ref this.searchResults, value);
-    }
-
     ICommand searchCommand;
     public ICommand SearchCommand {
       get => this.searchCommand;
       set => this.Set(ref this.searchCommand, value);
+    }
+
+    ObservableCollection<SearchResult> searchResults = new ObservableCollection<SearchResult>();
+    public ObservableCollection<SearchResult> SearchResults {
+      get => this.searchResults;
+      set => this.Set(ref this.searchResults, value);
     }
 
   }
